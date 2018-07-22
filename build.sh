d19a6ce5016f64f4ddd16ea1c21ddfe7bf88103a
chevris <tevachevris@gmail.com>
2018-07-22 23:03:55 +0200

    added: build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "** Cleaning up files...                 **"
+
+rm -rf .git
+rm .gitignore
+rm README.md
+rm package.json
+
+echo "** All done.                            **"
+echo "** Final theme ready for release        **"
+echo "**                                      **"
+echo "****************** DONE ******************"
+echo ""
