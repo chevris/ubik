3abe9fc7311ceed0ab8256b978fd674bd5ae93b0
chevris <tevachevris@gmail.com>
2019-01-16 14:11:02 +0100

    modified: css refactor

--- assets/js/all.min.js
+++ assets/js/all.min.js
@@ -1,1 +1,28 @@
-function ubikScrollToTop(){"use strict";$window.on("scroll",function(){$j(this).scrollTop()>100?$j("#scrollToTop").fadeIn():$j("#scrollToTop").fadeOut()})}jQuery(document).foundation();var $j=jQuery.noConflict();$window=$j(window),$j(document).on("ready",function(){"use strict";ubikScrollToTop()});
+// Foundation plugins instanciation
+
+jQuery(document).foundation();
+
+var $j = jQuery.noConflict();
+    $window = $j( window );
+
+$j( document ).on( 'ready', function() {
+	"use strict";
+	// Scroll to top
+	ubikScrollToTop();
+} );
+
+/**
+ * Scroll to top
+ */
+function ubikScrollToTop() {
+	"use strict"
+	
+	$window.on( 'scroll', function() {
+		if ( $j( this ).scrollTop() > 100 ) {
+			$j( '#scrollToTop' ).fadeIn();
+		} else {
+			$j( '#scrollToTop' ).fadeOut();
+		}
+	});
+
+}
